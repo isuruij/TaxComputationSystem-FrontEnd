98641cca67f35b11151cfc7739636bca7df060c8
isuru <2020uelog@gmail.com>
2024-01-23 17:35:06 +0530

    updated notification count to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Notification from "../assets/Notification.svg";
 import Profile from "../assets/Profile.svg";
+import { useState } from "react";
 
 function Header() {
+  useEffect(() => {
+    setcount(0);
+  }, []);
+
+  const [count, setcount] = useState(0);
+
   return (
-    <div style={{ backgroundColor: "#D3E9FE", display: "flex"}}>
-      
-      <img style={{marginLeft:"81%",paddingTop:"1%",paddingRight:"1%"}} src={Notification} alt="Notification" />
-      <span style={{marginLeft:"83%",marginTop:"1%"}} className="position-absolute translate-middle badge rounded-pill bg-danger">
-        0
-        <span className="visually-hidden">unread messages</span>
-      </span>
+    <div
+      style={{
+        boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",
+        backgroundColor: "#D3E9FE",
+        display: "flex",
+      }}
+    >
+      <img
+        style={{ marginLeft: "81%", paddingTop: "1%", paddingRight: "1%" }}
+        src={Notification}
+        alt="Notification"
+      />
+      {count != 0 && (
+        <span
+          style={{ marginLeft: "83%", marginTop: "1%" }}
+          className="translate-middle badge rounded-pill bg-danger"
+        >
+          {count}
+          {/* <span className="visually-hidden">unread messages</span> */}
+        </span>
+      )}
       <img src={Profile} alt="Profile" />
     </div>
   );
