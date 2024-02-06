5cf95bf83fd3c124f369d14a0c815f4b0ffd9036
Anupama Gunasekara <87498573+AnupamaGunasekara@users.noreply.github.com>
2024-02-06 12:59:35 +0530

    taxstatus, homepage, navbar update

--- src/components/user/Sidenavbar.jsx
+++ src/components/user/Sidenavbar.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-import reactlogo from "../../assets/react.svg";
-import home from "../../assets/home.svg";
-import changeincome from "../../assets/changeincome.svg";
-import DownloadTaxreport from "../../assets/DownloadTaxreport.svg";
-import viewtax from "../../assets/viewtax.svg";
-import taxhistory from "../../assets/taxhistory.svg";
-import registration from "../../assets/registration.svg";
-import logout from "../../assets/logout.svg";
-import Settings from "../../assets/Settings.svg";
 import { useNavigate } from "react-router-dom";
+import DownloadTaxreport from "../../assets/DownloadTaxreport.svg";
+import Settings from "../../assets/Settings.svg";
+import home from "../../assets/home.svg";
+import logout from "../../assets/logout.svg";
+import registration from "../../assets/registration.svg";
+import taxhistory from "../../assets/taxhistory.svg";
+import viewtax from "../../assets/viewtax.svg";
 
 function Sidenavbar() {
   const buttonStyle = {
@@ -39,7 +37,7 @@ function Sidenavbar() {
       <button type="button" className="btn btn-primary" style={buttonStyle}>
         <img
           src={home}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left",marginRight:"10px" }}
           alt="Icon"
         />
         Home
@@ -54,7 +52,7 @@ function Sidenavbar() {
       >
         <img
           src={registration}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left",marginRight:"10px" }}
           alt="Icon"
         />
         Registration
@@ -62,7 +60,7 @@ function Sidenavbar() {
       <button type="button" className="btn btn-primary" style={buttonStyle}>
         <img
           src={DownloadTaxreport}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left",marginRight:"10px" }}
           alt="Icon"
         />
         Download report
@@ -70,7 +68,7 @@ function Sidenavbar() {
       <button type="button" className="btn btn-primary" style={buttonStyle}>
         <img
           src={viewtax}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left" ,marginRight:"10px"}}
           alt="Icon"
         />
         viewtax
@@ -78,7 +76,7 @@ function Sidenavbar() {
       <button type="button" className="btn btn-primary" style={buttonStyle}>
         <img
           src={taxhistory}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left",marginRight:"10px" }}
           alt="Icon"
         />
         taxhistory
@@ -86,15 +84,20 @@ function Sidenavbar() {
       <button type="button" className="btn btn-primary" style={buttonStyle}>
         <img
           src={Settings}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left",marginRight:"10px" }}
           alt="Icon"
         />
         Settings
       </button>
-      <button type="button" className="btn btn-primary" style={buttonStyle}>
+      <button type="button" className="btn btn-primary" style={{ textAlign: "left",
+    display: "block",
+    marginBottom: "12px",
+    width: "90%",
+    marginLeft: "4%",
+    boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",marginTop:"100%"}}>
         <img
           src={logout}
-          style={{ alignItems: "left", textAlign: "left" }}
+          style={{ alignItems: "left", textAlign: "left",marginRight:"10px" }}
           alt="Icon"
         />
         logout
---
+++ src/components/user/TaxStatus/TaxStatusBody.jsx
@@ -0,0 +1,109 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+
+
+
+export default function TaxStatus() {
+  const now = 70;
+  const buttonStyle = {
+    textAlign: "Center",
+    display: "block",
+    marginBottom: "12px",
+    width: "200px",
+    marginLeft: "35%",
+    boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",
+  };
+
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+    validateDates(event.target.value, endDate);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+    validateDates(startDate, event.target.value);
+  };
+
+  const validateDates = (start, end) => {
+    const startDateObj = new Date(start);
+    const endDateObj = new Date(end);
+    
+
+    if (startDateObj > endDateObj) {
+      
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  };
+
+
+  return (
+    <div><div style={{
+        borderRadius: "15px",
+        padding: "20px 40px",
+        backgroundColor: "#D3E9FE",
+        width: "100%",
+        marginTop:"5px",
+        marginBottom:'20px',
+        boxShadow:"1px 5px 3px -3px rgba(0,0,0,0.44)",
+        height:"100vh"}}> 
+        <div style={{display:"flex", justifyContent:"space-between"}}>
+              <div className="form-group">
+                <label className="lables" for="birthday">
+                  Start Date
+                </label>
+                <div className="custom_input">
+                  <input
+                    className="details-input form-control"
+                    type="date"
+                    id="startDate"
+                    placeholder=""
+                    value={startDate} onChange={handleStartDateChange}
+                    
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label className="lables" for="birthday">
+                  End Date
+                </label>
+                <div className="custom_input">
+                  <input
+                    className="details-input form-control"
+                    type="date"
+                    id="endDate"
+                    placeholder=""
+                    value={endDate} onChange={handleEndDateChange}
+                  />
+                  {!isValid && <p style={{ color: 'red' }}>End date must be greater than the start date.</p>}
+
+                </div>
+              </div>
+              
+        </div>
+
+
+        <div>
+            
+        </div>
+        <div  style={{alignItems:'center',marginTop:"50px"}}>
+        <button type="button" className="btn btn-primary d-flex justify-content-center" style={buttonStyle}>
+              View Tax Status
+        </button>
+        </div>
+        <div style={{marginTop:"100px"}}>
+        <div style={{paddingTop:"10px"}}><ProgressBar now={now} label={`${now}%`} style={{boxShadow:"1px 5px 3px -3px rgba(0,0,0,0.44)"}}/></div>
+         <div style={{color:'#0085FF',textAlign:"center", textShadow:"2px 2px 4px rgba(0, 0, 0,0.3)",fontSize:'20px',marginTop:"20px"}}>This progress Bar shows the progress of the taxpayer</div> 
+         <div style={{color:'Gray',textAlign:"justify", textShadow:"2px 2px 4px rgba(0, 0, 0,0.3)",fontSize:'20px',marginTop:"40px"}}>The progress bar serves as a visual representation of the taxpayer's financial contributions over a specified period. As it dynamically adjusts, it mirrors the proportion of taxes paid by the user during the chosen timeframe, displaying the percentage completion. This intuitive tool provides a quick and accessible way for users to gauge their tax payment progress, fostering transparency and comprehension in financial matters.</div>
+        </div>
+      </div></div>
+  )
+}
