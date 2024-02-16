10680d9615f3f57a241c708685c565c3ec31ca6f
THIRUNIJAYATHILAKE <thiruninavodya18@gmail.com>
2024-02-16 14:49:31 +0530

    update

---
+++ src/pages/TaxHistory.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import SettingSubMenue from "../components/user/Settings/SettingSubMenue";
+import BasicDetails from "../components/user/Settings/BasicDetails";
+import Header from "../components/user/Header/Header";
+import Sidenavbar from "../components/user/Sidenavbar/Sidenavbar";
+import TaxPaymentHistory from "../components/user/TaxPaymentHistory/TaxPaymentHistory";
+import SettingSubMenueTax from "../components/user/Settings/SettingSubMenueTax";
+
+function TaxHistory() {
+  return (
+    <div>
+      <div>
+        <Header />
+        <div style={{ display: "flex" }}>
+          <div style={{ marginTop: "5px" }}>
+            <Sidenavbar />
+          </div>
+          <div style={{ marginLeft: "5px", marginTop: "5px" }}>
+            <div>
+              <SettingSubMenueTax />
+            </div>
+            <div style={{marginTop:"5px"}}>
+              <TaxPaymentHistory/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TaxHistory
