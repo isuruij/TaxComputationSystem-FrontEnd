e1b90b761f35ab9541e3c175420c7562ff5fd52d
THIRUNIJAYATHILAKE <thiruninavodya18@gmail.com>
2024-02-16 12:40:37 +0530

    updateDropdown

--- src/components/user/TaxPaymentHistory/TaxPaymentHistory.jsx
+++ src/components/user/TaxPaymentHistory/TaxPaymentHistory.jsx
@@ -5,23 +5,46 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function TaxPaymentHistory() {
+
   const [date, setDate] = useState()
 
   function dateValue(){
     (e)=>{setDate({...date,date:e.target.date})}
   }
+
+  
+    const [selectedValue, setSelectedValue] = useState(null);
+    const handleDropdownSelect = (value) => {
+      setSelectedValue(value);
+    }
+
   return (
 
-   
+    
     <div>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"></link>
 
       <div className="divbuttongroup">
-        <h1 style={{fontSize:50+'px'}}>TAX Payment History</h1>
+        <h1 style={{fontSize:30+'px', marginBottom:20+'px'}}>TAX Payment History</h1>
         <div className="divbutton">
-          <button type="button" className="btn btn-primary " style={{fontSize:20+'px'}}>Tax Status</button>
-          <button type="button" className="btn btn-primary " style={{fontSize:20+'px'}}>Tax History</button>
-          <button type="button" className="btn btn-primary " style={{fontSize:20+'px'}}>Tax Payment Update</button>
+          <button type="button" className="btn btn-primary " style={{fontSize:20+'px'}}>
+            <span className="icon">
+              <img src="" alt="" />
+            </span>
+            Tax Status
+          </button>
+          <button type="button" className="btn btn-primary " style={{fontSize:20+'px'}}>
+            <span className="icon">
+              <img src="" alt="" />
+            </span>
+            Tax History
+            </button>
+          <button type="button" className="btn btn-primary " style={{fontSize:20+'px'}}>
+          <span className="icon">
+              <img src="" alt="" />
+            </span>
+            Tax Payment Update
+            </button>
         </div>
       </div>
 
@@ -29,25 +52,26 @@ function TaxPaymentHistory() {
         <div className="dropdownPage">
           
 
+          
           <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{padding: 2 + 'px',fontSize:22 +'px'}}>
-              Paid Taxes
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{padding: 2 + 'px',fontSize:22 +'px',height:51+'px'}}>
+              {selectedValue || 'Paid Taxes'}
             </button>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#" onClick={() => handleDropdownSelect('Action')}>Action</a></li>
+              <li><a class="dropdown-item" href="#" onClick={() => handleDropdownSelect('Another action')}>Another action</a></li>
+              <li><a class="dropdown-item" href="#" onClick={() => handleDropdownSelect('Something else here')}>Something else here</a></li>
             </ul>
           </div>
 
           <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{padding: 2 + 'px',fontSize:22 +'px'}}>
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{padding: 2 + 'px',fontSize:22 +'px',height:51+'px'}}>
               All Transactions
             </button>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#" onClick={() => handleDropdownSelect('Action')}>Action</a></li>
+              <li><a class="dropdown-item" href="#" onClick={() => handleDropdownSelect('Another action')}>Another action</a></li>
+              <li><a class="dropdown-item" href="#" onClick={() => handleDropdownSelect('Something else here')}>Something else here</a></li>
             </ul>
           </div>
 
@@ -59,6 +83,7 @@ function TaxPaymentHistory() {
               type="date"
               id="Ddate"
               onChange={dateValue}
+              style={{height:51+'px'}}
             />
           </div>
         </div>
@@ -71,24 +96,23 @@ function TaxPaymentHistory() {
               type="date"
               id="Ddate"
               onChange={dateValue}
+              style={{height:51+'px'}}
             />
           </div>
         </div>
-         
-
-        
           
           
           
             
         
-
-          <button class="btn btn-primary" type="button"  aria-expanded="false" style={{padding: 2 + 'px',fontSize:20+'px'}}>
+        <div>
+          <button class="btn btn-primary d-inline-block" type="button"  aria-expanded="false" style={{padding: 2 + 'px',fontSize:20+'px',width:250+'px'}}>
               Serial Number
-            <button class="btn btn-primary " type="button"   aria-expanded="false"style={{fontSize:20+'px'}}>
+            <button class="btn btn-primary d-inline-block" type="button"   aria-expanded="false"style={{fontSize:20+'px'}}>
                 11569
             </button>
           </button>
+       </div>
         </div>
 
         <div className="divtable">
