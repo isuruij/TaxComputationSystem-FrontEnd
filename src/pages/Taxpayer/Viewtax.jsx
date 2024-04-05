929df0fd38f31c7d057d50ab551616317636fddf
isuruij <2020uelog@gmail.com>
2024-04-05 18:48:25 +0530

    Refactor file structure and update navigation paths

--- src/pages/Taxpayer/Viewtax.jsx
+++ src/pages/Taxpayer/Viewtax.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React from "react";
 import Header from "../../components/user/Header/Header";
 import Sidenavbar from "../../components/user/Sidenavbar/Sidenavbar";
 import { useState, useEffect } from "react";
 import Axios from "axios";
-import Taxview from "../../components/user/TaxView/TaxView"
-
+import Taxview from "../../components/user/TaxView/TaxView";
 
 function Viewtax() {
   return (
     <div>
-      <Header />
-      <div style={{ display: "flex" }}>
-        <div style={{marginTop:"5px"}}>
-          <Sidenavbar />
+      <div>
+        <div style={{ position: "fixed" }}>
+          <Header />
         </div>
-        <div style={{width:'65vw',marginLeft:"5px",marginTop:"5px"}} >
-          <Taxview/>
+        <div style={{ display: "flex" }}>
+          <div style={{ marginTop: "8.5vh", position: "fixed" }}>
+            <Sidenavbar />
+          </div>
+          <div style={{ marginLeft: "5px", marginTop: "5px" }}>
+            <div
+              style={{
+                marginLeft: "20vw",
+                marginTop: "7.7vh",
+                position: "fixed",
+              }}
+            ></div>
+            <div style={{ marginTop: "7.8vh", marginLeft: "20vw" }}>
+              <Taxview />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Viewtax
+export default Viewtax;
