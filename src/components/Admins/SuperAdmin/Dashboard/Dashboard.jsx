e688d870e6b54ca1f69e8e4efbd1538c2ad249c2
isuruij <2020uelog@gmail.com>
2024-06-22 23:10:53 +0530

    chore: Update width of Dashboard and Submissions components

--- src/components/Admins/SuperAdmin/Dashboard/Dashboard.jsx
+++ src/components/Admins/SuperAdmin/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
           borderRadius: "15px",
           padding: "20px 40px",
           backgroundColor: "#F3FFF5",
-          width: "100%",
+          width: "78vw",
           marginTop: "5px",
           marginBottom: "20px",
           boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",
