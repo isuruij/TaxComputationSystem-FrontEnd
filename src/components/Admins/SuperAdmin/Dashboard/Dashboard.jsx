f193313f2da95cc61445639dce1582bbf7c6d49b
isuruij <2020uelog@gmail.com>
2024-06-22 21:16:39 +0530

    updated

--- src/components/Admins/SuperAdmin/Dashboard/Dashboard.jsx
+++ src/components/Admins/SuperAdmin/Dashboard/Dashboard.jsx
@@ -1,25 +1,26 @@
-
-import React from 'react';
-import './SearchBar.css';
-import UserList from './UserList';
-
-
+import React from "react";
+import "./SearchBar.css";
+import UserList from "./UserList";
 
 export default function Dashboard() {
   return (
-    <div><div style={{
-        borderRadius: "15px",
-        padding: "20px 40px",
-        backgroundColor: "#F3FFF5",
-        width: "100%",
-        marginTop:"5px",
-        marginBottom:'20px',
-        boxShadow:"1px 5px 3px -3px rgba(0,0,0,0.44)",
-        height:"100vh"}}>
-
-            <div>
-              <UserList/>
-            </div>
-             </div></div>
-  )
+    <div>
+      <div
+        style={{
+          borderRadius: "15px",
+          padding: "20px 40px",
+          backgroundColor: "#F3FFF5",
+          width: "100%",
+          marginTop: "5px",
+          marginBottom: "20px",
+          boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <UserList />
+        </div>
+      </div>
+    </div>
+  );
 }
