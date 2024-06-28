42fa5847f731fa299ec60b33053be4dabf5d2c32
Thimira Deshaka <thimiradeshakashan0220@gmail.com>
2024-06-28 23:14:55 +0530

    last commit

--- src/components/DataEntry/Header/DHeader.jsx
+++ src/components/DataEntry/Header/DHeader.jsx
@@ -1,47 +1,43 @@
 import React, { useEffect } from "react";
-import Notification from "../../../assets/AdminNotification.svg";
+// import Notification from "../../../assets/adminNotification.svg";
 import Profile from "../../../assets/AdminProfile.svg";
 import { useState } from "react";
 import "./DHeader.css";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
-function Header(props) {
+function DHeader() {
+  const cookieValue = Cookies.get("token");
+  const name = jwtDecode(cookieValue).name;
+
   const [count, setcount] = useState(0);
   useEffect(() => {
     setcount(2);
-  });
+  }, []);
+
   return (
     <div
       style={{
-        // backgroundColor: "#049370"darkgreeen,
         backgroundColor: "#F3FFF5",
         display: "flex",
-        boxShadow: "1px 3px 2px 1px rgba(0,0,0,0.44)",
-        height: "5vh",
+        alignItems: "center",
+        boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",
+        width: "98.5vw",
+        justifyContent: "flex-end", // aligns items to the right
+        paddingTop: "1vh",
+        paddingBottom: "1vh",
       }}
     >
-      {/*This is notification logo */}
-      <img
-        style={{ marginLeft: "81%", paddingTop: "1%", paddingRight: "1%" }}
-        src={Notification}
-        alt="Notification"
-      />
-      {count != 0 ? (
-        <span
-          style={{ marginLeft: "83%", marginTop: "1.1%" }}
-          className="ncount position-absolute translate-middle badge rounded-pill bg-danger"
-        >
-          {count}
+      <div
+        style={{ marginRight: "20vw", display: "flex", alignItems: "center" }}
+      >
+        <img src={Profile} alt="Profile" />
+        <span style={{ marginLeft: "1vw" }}>
+          <h6 className="headername">{name}</h6>
         </span>
-      ) : (
-        <h6></h6>
-      )}
-      {/*This is Profile logo */}
-      <img src={Profile} alt="Profile" />
-      <span style={{ display: "flex", marginTop: "1%", marginLeft: "1%" }}>
-        <h6>{props.name}</h6>
-      </span>
+      </div>
     </div>
   );
 }
 
-export default Header;
+export default DHeader;
