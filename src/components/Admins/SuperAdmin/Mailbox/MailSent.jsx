a27974e897521c980f92a25fe7459928630e5208
isuruij <2020uelog@gmail.com>
2024-06-28 23:58:26 +0530

    chore: Add MailSent and Inbox components for SuperAdmin mailbox

---
+++ src/components/Admins/SuperAdmin/Mailbox/MailSent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SentList from './SentList'
+
+export default function MailSent() {
+  return (
+    <div><div className='custom-container'>
+    <div>
+      <SentList/>
+    </div>
+    </div></div>
+  )
+}
